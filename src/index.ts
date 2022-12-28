0b3920bd0c6ac9cc820abd8f0d3f785a29592f65
Martins Kayode <martinsk09@gmail.com>
2022-12-28 02:10:59 +0100

    Landing Page added

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import routes from './routes/index';
 const app = express();
 const port = 3000;
 
+
+app.get('/',routes); //provides access to the landing page
 app.use('/api', routes);
 //User uploads an image and specifies the width and height
 //After the image is processed the user is redirected to the resized thumb
